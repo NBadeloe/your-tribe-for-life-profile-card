0f1e01c00a5820521fbeda00a23d0f259cc693f5
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2022-09-08 20:58:50 +0200

    following svelte prismic tutorial

---
+++ profile-card/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import * as prismicH from '@prismicio/helpers';
+  export let document;
+</script>
+
+<main>
+  <div class="container">
+    <h1>Homepage</h1>
+    <pre>{JSON.stringify(document, null, 2)}</pre>
+  </div>
+</main>
+
+<style>
+  main {
+    font-family: sans-serif;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 20px;
+  }
+
+  .container > * {
+    width: 100%;
+    max-width: 700px;
+  }
+</style>
